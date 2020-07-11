6990b2f35bb7378ad274b4e5cc8f848798ae7781
muhdmirzamz <muhdmirzamz@gmail.com>
2020-07-11 14:23:21 +0800

    Updates

---
+++ precache-manifest.9186697282b27101f5c2c601ac325109.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6ac55738387cae11f45ad180efe8436b",
+    "url": "/ReactTodoList/index.html"
+  },
+  {
+    "revision": "924deebb9e579996a5d9",
+    "url": "/ReactTodoList/static/css/main.b38e3362.chunk.css"
+  },
+  {
+    "revision": "310f3e1a21d3400e960b",
+    "url": "/ReactTodoList/static/js/2.914feefa.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/ReactTodoList/static/js/2.914feefa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "924deebb9e579996a5d9",
+    "url": "/ReactTodoList/static/js/main.a4e8db2b.chunk.js"
+  },
+  {
+    "revision": "ae7e80044c7e50c610e0",
+    "url": "/ReactTodoList/static/js/runtime-main.4d85ca20.js"
+  }
+]);
